092ae69b7a5f9528c89e57abc13857bc8778f949
Samarth Swami <samarthswami66@gmail.com>
2025-12-15 13:55:23 +0530

    Create 3.Sum of Two Numbers.c

---
+++ 3.Sum of Two Numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,sum;
+    scanf("%d %d", &a,&b);
+    
+    sum= a+b;
+    
+    printf("The sum of %d and %d is %d.", a,b,sum);
+    
+    return 0;
+}
